717dace18dccd3030d45ac41389ef33e93ae6452
Marcos Wlrich <marcosapw.dev@gmail.com>
2022-01-24 00:42:31 -0300

    Config de arquivos enviados

--- src/controllers/FotoControllers.js
+++ src/controllers/FotoControllers.js
@@ -1,6 +1,18 @@
+import multer from 'multer';
+import multerconfig from '../config/multerConfig';
+
+const upload = multer(multerconfig).single('foto');
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, err => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controllers/FotoControllers';
-import multerconfig from '../config/multerConfig';
-
-const upload = multer(multerconfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
